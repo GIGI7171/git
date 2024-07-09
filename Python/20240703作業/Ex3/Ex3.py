c16745e53f890adaf4420d3350b90b2a80376ca4
GIGI <a199011111111@gmail.com>
2024-07-09 15:47:16 +0800

    新增檔案

---
+++ Python/20240703作業/Ex3/Ex3.py
@@ -0,0 +1,8 @@
+while True:
+    y = int(input())
+    if y == 0:
+        break
+    elif y % 10 == 0:
+        print(f"{y} 能被10整除，商:{int(y/10)}, 餘:{y%10}")
+    else:
+        print(f"{y} 不能被10整除，商:{int(y/10)}, 餘:{y%10}")
